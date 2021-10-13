2b3cd915b9c55c74babf2fa2f0190feae492dfc4
vedantfire00 <83462470+vedantfire00@users.noreply.github.com>
2021-10-13 17:45:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,11 +13,40 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    posenet = ml5.poseNet(video, ml);
+    posenet.on("pose", gotpo);
+}
+
+function ml()
+{
+    console.log("model loaded");
+}
+rwx = 0;
+rwy = 0;
+lwx = 0;
+lwy = 0;
+
+function gotpo(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+        rwx= results[0].pose.rightWrist.x;
+        rwy= results[0].pose.rightWrist.y;
+        lwy= results[0].pose.leftWrist.y;
+        lwx= results[0].pose.leftWrist.x;
+        console.log("right Wrist X " + rwx + " right wrist Y " + rwy);
+        console.log("left Wrist X " + lwx + " left wrist Y " + lwy);
+
+    }
 }
 
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(2);
 }
 
 
