b7258bcef434cde7ee0e0e302913d805a69ad092
vedantfire00 <83462470+vedantfire00@users.noreply.github.com>
2021-10-14 17:57:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,18 +27,24 @@ rwy = 0;
 lwx = 0;
 lwy = 0;
 
+srw=0;
+slw=0;
+
+
 function gotpo(results)
 {
     if(results.length>0)
     {
-        console.log(results);
-        rwx= results[0].pose.rightWrist.x;
-        rwy= results[0].pose.rightWrist.y;
-        lwy= results[0].pose.leftWrist.y;
+        console.log(results)
+        rwx= results[0].pose.rightWrist.x; 
+        rwy= results[0].pose.rightWrist.y; 
+        lwy= results[0].pose.leftWrist.y; 
         lwx= results[0].pose.leftWrist.x;
-        console.log("right Wrist X " + rwx + " right wrist Y " + rwy);
+        console.log("right Wrist X " + rwx + " right wrist Y " + rwy); 
         console.log("left Wrist X " + lwx + " left wrist Y " + lwy);
-
+        srw= results[0].pose.keypoints[10].score;
+        slw= results[0].pose.keypoints[9].score;
+        console.log("score of left wrist = " + slw + "score of right wrist = " + srw);
     }
 }
 
@@ -50,13 +56,13 @@ function play()
 }
 
 
-
-
-
-
-
-
 function draw()
 {
     image(video, 0, 0, 600, 500);
+    fill("red");
+    stroke("red");
+    if(slw>0.2)
+    {
+        circle(rightWrist)
+    }
 }
